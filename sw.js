166b9c4379817c0a8b318275cde9fd9a8fbfd210
asul05202-hue <asul05202@gmail.com>
2026-07-27 21:51:20 -0600

    init: repo limpio sin historial pesado

---
+++ sw.js
@@ -0,0 +1,11 @@
+// SW DESACTIVADO
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+    .then(() => self.clients.matchAll({includeUncontrolled:true}))
+    .then(clients => clients.forEach(c => c.navigate(c.url)))
+    .then(() => self.clients.claim())
+  );
+});
+// Sin interceptar nada
